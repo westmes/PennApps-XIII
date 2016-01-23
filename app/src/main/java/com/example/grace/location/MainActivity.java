12c8d75d6ee40d90ac8704a27f52638780a84c9a
akahd <dyunwen@seas.upenn.edu>
2016-01-23 10:27:35 -0500

    UI_part1

--- app/src/main/java/com/example/grace/location/MainActivity.java
+++ app/src/main/java/com/example/grace/location/MainActivity.java
@@ -1,30 +1,179 @@
 package com.example.grace.location;
 
-import android.content.Intent;
+import android.Manifest;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends FragmentActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    TextView textView1;
-    TextView textView2;
-    Intent map_int;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    protected static final String TAG = "Your Current Location";
+    double mLatitude;
+    double mLongitude;
+    private Location mLastLocation;
+    private Date date;
+
+    DateFormat fmtDateAndTime= DateFormat.getDateTimeInstance();
+    TextView dateAndTimeLabel;
+    Calendar dateAndTime = Calendar.getInstance();
+
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            dateAndTime.set(Calendar.YEAR, year);
+            dateAndTime.set(Calendar.MONTH, monthOfYear);
+            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
+    TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay,
+                              int minute) {
+            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            dateAndTime.set(Calendar.MINUTE, minute);
+            updateLabel();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent map_int = getIntent();
-        double mLongtitude = map_int.getDoubleExtra("LOCAL1",1);
-        double mLatitude = map_int.getDoubleExtra("LOCAL2",0);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
-        TextView textView1 = (TextView) findViewById(R.id.textView1);
-        TextView textView2 = (TextView) findViewById(R.id.textView2);
-        textView1.setText(String.valueOf(mLatitude));
-        textView2.setText(String.valueOf(mLongtitude));
+        buildGoogleApiClient();
 
+        Button btn=(Button)findViewById(R.id.date);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new DatePickerDialog(MainActivity.this,
+                        d,
+                        dateAndTime.get(Calendar.YEAR),
+                        dateAndTime.get(Calendar.MONTH),
+                        dateAndTime.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        btn=(Button)findViewById(R.id.time);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new TimePickerDialog(MainActivity.this, t, dateAndTime.get(Calendar.HOUR_OF_DAY), dateAndTime.get(Calendar.MINUTE), true).show();
+            }
+        });
+
+        dateAndTimeLabel=(TextView)findViewById(R.id.dateAndTime);
+        updateLabel();
+        Log.d("time", String.valueOf(date));
 
     }
+
+    private void updateLabel() {
+        dateAndTimeLabel.setText(fmtDateAndTime.format(dateAndTime.getTime()));
+    }
+
+    private synchronized void buildGoogleApiClient(){
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        Log.d("abc","abc");
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            Log.d("MLOCATION",String.valueOf(mLastLocation));
+
+            if (mLastLocation != null) {
+                mLatitude = mLastLocation.getLatitude();
+                mLongitude = mLastLocation.getLongitude();
+
+                LatLng your_location = new LatLng(mLongitude, mLatitude);
+                mMap.getUiSettings().setZoomControlsEnabled(true);
+                mMap.addMarker(new MarkerOptions().position(your_location).title(TAG).draggable(true));
+
+                Log.d("edf","edf");
+                Log.d("La", String.valueOf(mLatitude));
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+
+        if(result.hasResolution()){
+            try{
+                result.startResolutionForResult(this,0);
+            }catch (IntentSender.SendIntentException e) {
+
+            }
+        } else
+         Log.d("FAILED", "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
 }
+
+
